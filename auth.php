8520b997e4a87587a64cc5e70cd38c04e3101479
DevinClark <dclarkdesign@gmail.com>
2013-01-06 00:27:56 -0600

    Finished Authentication and started on getting daily summary. Daily summary does not work at this time.

--- auth.php
+++ auth.php
@@ -1,26 +1,54 @@
 <?php
 
-$data = array("email" => strtolower($_POST['email']), "pwd" => $_POST['pwd'], "service" => "nudge");
-$data_string = json_encode($data);
+$auth_result = null;
+$token = null;
+$xid = null;
 
-$ch = curl_init('https://jawbone.com/user/signin/login');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-	'Content-Type: application/json',
-	'Content-Length: ' . strlen($data_string))
+function getRequestData($url, $data) {
+
+	$options = array(
+		'http' => array(
+			'method'  => 'POST',
+			'header'  => 'Content-type: application/x-www-form-urlencoded',
+			'content' => http_build_query($data)
+		)
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+
+	$result = json_decode($result, true);
+
+	return $result;
+}
+
+$url = 'https://jawbone.com/user/signin/login';
+$data = array(
+	"email" => strtolower($_POST['email']),
+	"pwd" => $_POST['pwd'],
+	"service" => "nudge"
 );
-$result = curl_exec($ch);
+
+$auth_result = getRequestData($url, $data);
+
+if(isset($auth_result['token'])) {
+
+	$token = $auth_result['token'];
+	$xid = $auth_result['user']['xid'];
+}
 
 
+$data = array(
+	"date" => date("Y-n-j"),
+	"timezone" => -28800,
+	"move_goal" => 0,
+	"sleep_goal" => 0,
+	"eat_goal" => 0,
+	"check_levels" => 1,
+	"_token" => $token
+);
+$daily_summary = getRequestData("https://jawbone.com/nudge/api/users/$xid/healthCredits", $data);
 
-$info = curl_getinfo($ch);
-curl_close($ch);
-
-echo "<pre>"; print_r($result); echo "</pre>";
-echo "<pre>"; print_r($info); echo "</pre>";
+echo "<pre>"; print_r($daily_summary); echo "</pre>";
 
 
 
